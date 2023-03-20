e7dc1f93d0f34779a9c5bdde878d1fea19a8ebd1
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-03-20 13:18:12 -0300

    add functions to start the game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-(function main() {})();
+function initGame() {
+  const $play = document.querySelector(".btn");
+  $play.addEventListener("click", () => {
+    updateState("Tu turno de jugar");
+    renderRounds();
+    listenSquareClick();
+  });
+}
+
+function listenSquareClick() {
+  const $squares = document.querySelectorAll(".square");
+  $squares.forEach(($square) => {
+    $square.addEventListener("click", () => {});
+  });
+}
+
+function updateState(state) {
+  const $state = document.querySelector(".state");
+  $state.textContent = state;
+}
+
+function renderRounds() {
+  const $rounds = document.querySelector(".rounds");
+  const numberOfRounds = Number($rounds.textContent);
+  $rounds.textContent = numberOfRounds + 1;
+}
+
+(function main() {
+  initGame();
+})();
