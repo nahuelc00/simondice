c221baea2e636dc47d968947dd08c4465dcee47d
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-03-20 13:35:47 -0300

    add functionality when clicking on square

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 function initGame() {
   const $play = document.querySelector(".btn");
-  $play.addEventListener("click", () => {
+  $play.addEventListener("click", (e) => {
     updateState("Tu turno de jugar");
     renderRounds();
     listenSquareClick();
+    e.target.setAttribute("disabled", "");
   });
 }
 
 function listenSquareClick() {
   const $squares = document.querySelectorAll(".square");
   $squares.forEach(($square) => {
-    $square.addEventListener("click", () => {});
+    $square.addEventListener("click", () => {
+      highlightSquare($square);
+    });
   });
 }
 
 function updateState(state) {
   const $state = document.querySelector(".state");
   $state.textContent = state;
+}
+
+function highlightSquare($square) {
+  $square.classList.remove("opacity-50");
+  $square.classList.add("opacity-100");
+  setTimeout(() => {
+    $square.classList.remove("opacity-100");
+    $square.classList.add("opacity-50");
+  }, 600);
 }
 
 function renderRounds() {
