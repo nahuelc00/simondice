5cbd3b5120f57f8e0bc7d20d95401392dd04291d
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-03-22 22:57:47 -0300

    add functionality for turns handler

--- src/index.js
+++ src/index.js
@@ -1,20 +1,62 @@
+let cpuHistory = [];
+let userHistory = [];
+
 function initGame() {
   const $play = document.querySelector(".btn");
-  $play.addEventListener("click", (e) => {
-    updateState("Tu turno de jugar");
-    renderRounds();
+
+  $play.addEventListener("click", () => {
+    updateState("Turno de la máquina");
+    handlerCpuTurn();
     listenSquareClick();
-    e.target.setAttribute("disabled", "");
   });
 }
 
-function listenSquareClick() {
+function handlerCpuTurn() {
+  desactivateInput();
+  const numRandom = getRandomNumber();
   const $squares = document.querySelectorAll(".square");
-  $squares.forEach(($square) => {
-    $square.addEventListener("click", () => {
-      highlightSquare($square);
+  const squareHighlight = $squares[numRandom];
+
+  cpuHistory.push(squareHighlight);
+
+  if (cpuHistory.length === 1) {
+    highlightSquare(squareHighlight);
+  } else {
+    cpuHistory.forEach(($square, index) => {
+      const ms = (index + 1) * 1000;
+      setTimeout(() => {
+        highlightSquare($square);
+      }, ms);
     });
+  }
+
+  setTimeout(() => {
+    activateInput();
+    updateState("Tu turno");
+  }, (cpuHistory.length + 1) * 500);
+}
+
+function listenSquareClick() {
+  const $squaresCont = document.querySelector(".grid");
+
+  $squaresCont.addEventListener("click", (e) => {
+    const $square = e.target;
+    highlightSquare($square);
+    cpuHistory.forEach((element) => {});
   });
+}
+
+function activateInput() {
+  document.querySelector("body").style = "";
+}
+
+function desactivateInput() {
+  document.querySelector("body").style = "pointer-events:none;";
+}
+
+function getRandomNumber() {
+  const numberRandom = Math.floor(Math.random() * 4);
+  return numberRandom;
 }
 
 function updateState(state) {
